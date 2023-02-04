fb44d6009aefd9225c68eb6d3e34d1017a636a99
Ivis Vinicius <ivcandido@protonmail.com>
2023-02-03 23:05:40 -0300

    Fim aula anatomia de classes

---
+++ java-anatomia-classes/src/edu/diojava/sintaxejava/MinhaClasse.java
@@ -0,0 +1,24 @@
+package edu.diojava.sintaxejava;
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+        //Impressão de texto na tela
+        System.out.println("Primeira classe criada com método executável");
+
+        //keyword final impede mudança no valor da variável
+        final String BR = "Brasil";
+
+        String primeiroNome = "Ivis";
+        String segundoNome = "Vinícius";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+    
+}
